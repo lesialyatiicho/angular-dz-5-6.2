5325e1da9d611c54c71792750c5bb7a672681055
Lesia Lyatiichko <tsivenkovalesia2004@gmail.com>
2025-12-06 10:00:39 +0000

    Updated resume component with pipes and directives

--- src/app/resume/resume.ts
+++ src/app/resume/resume.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule,
+  NgClass,
+  NgStyle,
+  UpperCasePipe,
+  LowerCasePipe,
+  DatePipe,
+  CurrencyPipe,
+  PercentPipe,} from '@angular/common';
 
 @Component({
   selector: 'app-resume',
   standalone: true,
+  imports: [CommonModule,
+    NgClass,
+    NgStyle,
+    UpperCasePipe,
+    LowerCasePipe,
+    DatePipe,
+    CurrencyPipe,
+    PercentPipe],
   templateUrl: './resume.html',
   styleUrl: './resume.css',
 })
@@ -24,4 +39,22 @@ export class Resume {
   ];
 
   photoUrl = 'img/photo-me.jpg';
-}
+
+
+  today = new Date();          // для DatePipe
+  isHighlighted = true;        // для ngClass
+  cardPadding = '24px';        // для ngStyle
+  nameColor = 'darkblue';      // для ngStyle
+
+
+  salary = 1800;          // пример для CurrencyPipe
+  progress = 0.75;        // пример для PercentPipe
+
+
+  showEnglish = true;
+
+
+  languages = ['English', 'Ukrainian', 'Russian'];
+
+
+  englishLevel: 'A2' | 'B1' | 'B2' = 'B2'}
